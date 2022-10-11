61459b5a016728e307f288d716bb9c6c86dbd7f4
andreasgasteiger <B00158249@mytudublin.ie>
2022-10-11 19:15:44 +0200

    finished challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float decaytime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && decaytime<=0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            decaytime = 1;
+        }
+        if (decaytime >= 0)
+        {
+            decaytime -= Time.deltaTime;
+        }
+    }
+}
